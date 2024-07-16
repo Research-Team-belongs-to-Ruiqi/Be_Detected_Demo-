bf2a14607b6766e18f880b46c986d9e5b9ab738b
Wang <163494349+Ruiqi370@users.noreply.github.com>
2024-07-16 13:58:08 +0800

    this is test for trailing punctuation.?!

--- Java-master/ServletAjax/src/com/hjy/SearchServlet.java
+++ Java-master/ServletAjax/src/com/hjy/SearchServlet.java
@@ -2,7 +2,7 @@
 // 添加一行新代码
 // 添加另一行新代码
 System.out.println("More changes for demonstration.");
-
+//this is test for distinguish the commas and periods.
 System.out.println("Initial change for demonstration.");
   /**
    * ��ȡ��������
@@ -13,7 +13,7 @@ System.out.println("Initial change for demonstration.");
   //test for abbreviation and jargon words.
   //just test
   //test for incorrect format
-  
+
   private List<String> getData(String keyword) {
     List<String> list = new ArrayList<String>();
     for (String data : datas) {
