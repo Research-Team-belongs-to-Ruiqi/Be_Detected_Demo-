3ed81bca74b7733ebb1d2078bfaa70e55ef891f6
Wang <163494349+Ruiqi370@users.noreply.github.com>
2025-03-02 17:47:04 +0800

    revise for test force push

--- Java-master/checkcode/src/com/servlet/ImageCodeServlet.java
+++ Java-master/checkcode/src/com/servlet/ImageCodeServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+print("this is test for detect force push")
 @WebServlet("/servlet/ImageCodeServlet")
 public class ImageCodeServlet extends HttpServlet {
 //add new login feature
